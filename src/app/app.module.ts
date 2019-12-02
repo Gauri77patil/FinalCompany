b0d32991e109218ce1af3d6216239dd35bca6d3d
gouri <gauri77patil@gmail.com>
2019-12-02 16:01:08 +0400

    my commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BluetoothSpeakerComponent } from './bluetooth-speaker/bluetooth-speaker.component';
+import {RoundProgressModule} from 'angular-svg-round-progressbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BluetoothSpeakerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RoundProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
